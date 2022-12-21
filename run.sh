cfc1e7df0942e889e88e2dbc79302ad500fac9ef
Margaret Fleck <margaret@Margarets-MacBook-Pro.local>
2022-12-21 13:19:16 -0500

    Add port arg to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 service=$1
+port=$2
 ./gradlew $service:quarkusBuild
 sudo docker build -t $service -f modules/$service/Dockerfile .
-sudo docker run -it -p 8080:8080 $service
+sudo docker run -it -p $port:8080 $service
 
